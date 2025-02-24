a604ebeb03aeb2a651e21348031ed2b190fdae02
Quietseong <105484422+Quietseong@users.noreply.github.com>
2025-02-25 06:29:34 +0900

    Update app.js

--- Dacon/2312_PriceModeling/app.js
+++ Dacon/2312_PriceModeling/app.js
@@ -3,7 +3,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/Squirrels_15746293266160/Dashboard1?:display_count=y&:origin=viz_share_link";
+const url = "https://public.tableau.com/shared/B92P3WTNS?:display_count=n&:origin=viz_share_link";
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
